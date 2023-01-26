2cc3ce116b1476906b72028895adac7baee06345
BuildTools <unconfigured@null.spigotmc.org>
2023-01-26 09:30:07 +0900

    firstCommit

---
+++ src/main/java/nankotsu029/spaceinbottle/SystemCore.java
@@ -0,0 +1,17 @@
+package nankotsu029.spaceinbottle;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SystemCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
