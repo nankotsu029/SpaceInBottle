f247344d155c15916d8d1b558e282e175548ee90
nankotsu029 <53854363+nankotsu029@users.noreply.github.com>
2026-06-04 11:16:48 +0900

    Delete build_mac.sh

--- build_mac.sh
+++
@@ -1,4 +0,0 @@
-echo "start processing"
-mvn install
-cp target/Space-in-bottle-1.0-SNAPSHOT.jar /Users/temma/Desktop/minecraft_files/minecraftServers/VSFN-1.19.3/plugins/Space-in-bottle-1.0-SNAPSHOT.jar
-echo "Success of all processes"
